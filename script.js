56f42a4398d82cc4e8ba50fea5375ab84914a8cd
dcalero <dcalero@inti.gob.ar>
2023-01-20 16:08:42 -0300

    structure

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const photo = document.getElementById('photo'); 
-
 fetch('https://randomuser.me/api/')
   .then(response => response.json())
   .then(data => {const UserData = data.results[0];
-    photo.src = UserData.picture.large;
+    $('#photo').attr('src', UserData.picture.large);
+    $('#name').text(`${UserData.name.first} ${UserData.name.last}`);
+    $('#email').text(`E-mail: ${UserData.email}`);
+    $('#phone').text(`Phone: ${UserData.phone}`);
+    $('#address').text(`${UserData.location.city} ${UserData.location.state}, ${UserData.location.country}`);
+
     console.log(UserData);
 
-
 });
+
+
 
